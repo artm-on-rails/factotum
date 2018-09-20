52bb8d068895295f6a4a7585861362911ed90f98
Artem Baguinski <abaguinski@gmail.com>
2018-09-20 14:48:01 +0200

    Seed some trades

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
+Jack.destroy_all
 Jack.create(
   email: "jack@trades.com",
   password: "letmein"
 )
+
+Trade.destroy_all
+Trade.create(name: "Tailor")
+Trade.create(name: "Reaper")
+Trade.create(name: "Pipe player")
