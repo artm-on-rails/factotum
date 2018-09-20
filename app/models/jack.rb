532576479a29bbc2eab59655d6b75beba669a9e4
Artem Baguinski <abaguinski@gmail.com>
2018-09-20 14:17:59 +0200

    Generate Jack model
        rails g devise Jack

---
+++ app/models/jack.rb
@@ -0,0 +1,6 @@
+class Jack < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
