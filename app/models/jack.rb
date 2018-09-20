e8a894b9c2fe17e8af875e152b579c48030b0d67
Artem Baguinski <abaguinski@gmail.com>
2018-09-20 16:46:58 +0200

    Add Jack#mastered_trades association and Jack#is_master_of? method

--- app/models/jack.rb
+++ app/models/jack.rb
@@ -1,9 +1,19 @@
 class Jack < ApplicationRecord
   devise :database_authenticatable
   has_many :occupations
+  has_many :mastered_occupations,
+           -> { where master: true },
+           class_name: "Occupation"
   has_many :trades, through: :occupations
+  has_many :mastered_trades,
+           through: :mastered_occupations,
+           source: :trade
 
   def of_all_trades?
     email[/factotum/].present?
+  end
+
+  def is_master_of? trade
+    of_all_trades? || mastered_trades.exists?(trade.id)
   end
 end
--- test/models/jack_test.rb
+++ test/models/jack_test.rb
@@ -13,7 +13,21 @@ class JackTest < ActiveSupport::TestCase
 
   test "Jack isn't master of his trades by default" do
     tailor = create(:tailor)
-    jack = create(:jack, trades: [tailor])
-    refute jack.occupations.first.master
+    jack = create(:jack)
+    jack.trades = [tailor]
+    refute jack.is_master_of?(tailor)
+  end
+
+  test "Jack is master of his mastered trades" do
+    tailor = create(:tailor)
+    jack = create(:jack)
+    jack.mastered_trades = [tailor]
+    assert jack.is_master_of?(tailor)
+  end
+
+  test "Factotum is master of any trade" do
+    tailor = create(:tailor)
+    johannes = create(:factotum)
+    assert johannes.is_master_of?(tailor)
   end
 end
