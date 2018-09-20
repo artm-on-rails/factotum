2c32b483a8a9980003ef6c693316e7099126e2e1
Artem Baguinski <abaguinski@gmail.com>
2018-09-20 16:07:27 +0200

    Add and test :factotum factory

--- test/factories/jacks.rb
+++ test/factories/jacks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :jack do
     sequence(:email) { |n| "jack_of_#{n}@trades.com"}
+
+    factory :factotum do
+      sequence(:email) { |n| "johannes.factotum_#{n}@trades.com"}
+    end
   end
 end
--- test/models/jack_test.rb
+++ test/models/jack_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class JackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Jack is not of all trades by default" do
+    jack = create(:jack)
+    refute jack.of_all_trades?
+  end
+
+  test "Factotum is of all trades by default" do
+    johannes = create(:factotum)
+    assert johannes.of_all_trades?
+  end
 end
