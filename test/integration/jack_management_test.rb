f78cc1548f9626e3d4a6bdad2f9d8db18aec92e6
Artem Baguinski <abaguinski@gmail.com>
2018-09-24 14:18:19 +0200

    Test that Jack of all trades can create new Jacks

--- test/integration/jack_management_test.rb
+++ test/integration/jack_management_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class JackManagementTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @jack_of_all_trades = create(:jack, :of_all_trades)
+  end
+
+  test "Jack of all trades can create new Jacks" do
+    sign_in(@jack_of_all_trades)
+    get "/jacks/new"
+    assert_response :success
+    post "/jacks", params: {
+      jack: {
+        email: "new_jack@trades.org",
+        password: "secret",
+        password_confirmation: "secret"
+      }
+    }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "#notice", "Jack was successfully created."
+  end
 end
