367faae1d93b7161e0b19f60b6d5d811f8b144f9
Artem Baguinski <abaguinski@gmail.com>
2018-09-27 11:19:59 +0200

    Fix trade master's ability
    Should only be able to update the mastered trades

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(jack)
     return unless jack.is_master_of_some?
     can %i[read update], Jack
-    can %i[read update], Trade
+    can %i[read update], Trade, id: jack.mastered_trade_ids
     can :manage, Occupation, trade_id: jack.mastered_trade_ids
     return unless jack.of_all_trades?
     can :manage, :all
