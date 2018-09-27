14df52d11d2ecf64340c607237ea679696e76743
Artem Baguinski <abaguinski@gmail.com>
2018-09-27 15:09:24 +0200

    Add tooltip to destroy occupation link

--- app/decorators/occupation_decorator.rb
+++ app/decorators/occupation_decorator.rb
@@ -3,4 +3,8 @@ module OccupationDecorator
     master = self.master ? "Master " : ""
     "#{jack.email} is a #{master}#{trade.name}"
   end
+
+  def destroy_tooltip
+    "Unoccupy #{jack.email} from #{trade.name} trade"
+  end
 end
