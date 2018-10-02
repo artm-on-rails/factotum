8dc30dac5ee8d531a774ace3c3d32adc146f92f2
Artem Baguinski <abaguinski@gmail.com>
2018-10-02 15:50:34 +0200

    Hide tooltips before turbolinks caches the page

--- app/assets/javascripts/data-tooltip-handler.js
+++ app/assets/javascripts/data-tooltip-handler.js
@@ -1,3 +1,7 @@
 $(document).on("turbolinks:load", function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(document).on("turbolinks:before-cache", function() {
+  $('[data-toggle="tooltip"]').tooltip("hide");
+});
