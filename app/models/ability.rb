1a5c33facbad40369c4782a4af8a0d124d6787b9
Artem Baguinski <abaguinski@gmail.com>
2018-10-02 13:26:42 +0200

    Disable or hide uneditable form inputs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(jack)
-    can %i[read update], Jack, id: jack.id
+    alias_action :edit_details, to: :update_details
+    can %i[read update update_details], Jack, id: jack.id
     return unless jack.is_master_of_some?
     can %i[read update], Jack
+    cannot :update, Jack do |other|
+      other.of_all_trades?
+    end
     can %i[read update], Trade, id: jack.mastered_trade_ids
     can :manage, Occupation, trade_id: jack.mastered_trade_ids
     cannot %i[create update destroy], Occupation, jack_id: jack.id
